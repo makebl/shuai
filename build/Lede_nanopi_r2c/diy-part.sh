08e5121925251a3bf92ce3ae38204d1eadcf5819
马可 <78850005+makebl@users.noreply.github.com>
2022-07-22 21:35:19 +0800

    Update diy-part.sh

--- build/Lede_nanopi_r2c/diy-part.sh
+++ build/Lede_nanopi_r2c/diy-part.sh
@@ -7,8 +7,9 @@
 # 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
 # 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
 
-src-git dahuilang https://github.com/makebl/openwrt-package.git
-svn co https://github.com/shidahuilang/OpenClash/trunk luci-app-xiaolaoshu
+
+svn co https://github.com/shidahuilang/OpenClash package/luci-app-xiaolaoshu
+
 cat >$NETIP <<-EOF
 uci set network.lan.ipaddr='192.168.2.1'                      # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                   # IPv4 子网掩码
